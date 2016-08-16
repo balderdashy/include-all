0b8a92c6c668c6aab5cd9f30c66e34ebef55c772
Mike McNeil <mike@balderdash.co>
2016-08-15 23:50:07 -0500

    Wrapped up test in mocha for CI.

--- test/test.js
+++ test/test.js
@@ -7,69 +7,76 @@ var includeAll = require('../');
 
 
 
+describe('basic usage', function(){
+
+  it('should have loaded stuff as expected', function (){
+
+    var controllers = includeAll({
+      dirname: __dirname + '/controllers',
+      filter: /(.+Controller)\.js$/
+    });
+
+    assert.deepEqual(controllers, {
+      'main-Controller': {
+        index: 1,
+        show: 2,
+        add: 3,
+        edit: 4
+      },
+
+      'other-Controller': {
+        index: 1,
+        show: 'nothing'
+      }
+    });
 
 
-var controllers = includeAll({
-  dirname: __dirname + '/controllers',
-  filter: /(.+Controller)\.js$/
-});
+    if (process.version > 'v0.6.0') {
+      var mydir = includeAll({
+        dirname: __dirname + '/mydir',
+        filter: /(.+)\.(js|json)$/
+      });
 
-assert.deepEqual(controllers, {
-  'main-Controller': {
-    index: 1,
-    show: 2,
-    add: 3,
-    edit: 4
-  },
-
-  'other-Controller': {
-    index: 1,
-    show: 'nothing'
-  }
-});
-
-
-if (process.version > 'v0.6.0') {
-  var mydir = includeAll({
-    dirname: __dirname + '/mydir',
-    filter: /(.+)\.(js|json)$/
-  });
-
-  assert.deepEqual(mydir, {
-    foo: 'bar',
-    hello: { world: true, universe: 42 },
-    sub: {
-      config: { settingA: 'A', settingB: 'B' },
-      yes: true
+      assert.deepEqual(mydir, {
+        foo: 'bar',
+        hello: { world: true, universe: 42 },
+        sub: {
+          config: { settingA: 'A', settingB: 'B' },
+          yes: true
+        }
+      });
     }
-  });
-}
 
-var unfiltered = includeAll({
-  dirname: __dirname + '/filterdir',
-  filter: /(.+)\.js$/
-});
+    var unfiltered = includeAll({
+      dirname: __dirname + '/filterdir',
+      filter: /(.+)\.js$/
+    });
 
-assert(unfiltered['.svn']);
-assert(unfiltered['root']);
-assert(unfiltered['sub']);
+    assert(unfiltered['.svn']);
+    assert(unfiltered['root']);
+    assert(unfiltered['sub']);
 
-var excludedSvn = includeAll({
-  dirname: __dirname + '/filterdir',
-  filter: /(.+)\.js$/,
-  excludeDirs: /^\.svn$/
-});
+    var excludedSvn = includeAll({
+      dirname: __dirname + '/filterdir',
+      filter: /(.+)\.js$/,
+      excludeDirs: /^\.svn$/
+    });
 
-assert.equal(excludedSvn['.svn'], undefined);
-assert.ok(excludedSvn['root']);
-assert.ok(excludedSvn['sub']);
+    assert.equal(excludedSvn['.svn'], undefined);
+    assert.ok(excludedSvn['root']);
+    assert.ok(excludedSvn['sub']);
 
-var excludedSvnAndSub = includeAll({
-  dirname: __dirname + '/filterdir',
-  filter: /(.+)\.js$/,
-  excludeDirs: /^(\.svn|sub)$/
-});
+    var excludedSvnAndSub = includeAll({
+      dirname: __dirname + '/filterdir',
+      filter: /(.+)\.js$/,
+      excludeDirs: /^(\.svn|sub)$/
+    });
 
-assert.equal(excludedSvnAndSub['.svn'], undefined);
-assert.ok(excludedSvnAndSub['root']);
-assert.equal(excludedSvnAndSub['sub'], undefined);
+    assert.equal(excludedSvnAndSub['.svn'], undefined);
+    assert.ok(excludedSvnAndSub['root']);
+    assert.equal(excludedSvnAndSub['sub'], undefined);
+
+  });//</it should have loaded stuff as expected>
+
+});//</describe>
+
