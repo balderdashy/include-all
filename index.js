4404a13590571f415af7471cef3af02bebc2c5c5
Mike McNeil <mike@balderdash.co>
2016-09-14 20:02:21 -0500

    Added experimental method: scanSync().

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ module.exports = function includeAllSync(options) {
 
 
 //////////////////////////////////////////////////////////////////////////////
-// The rest of the methods below are all originally from sails-build-dictionary.
+// The four methods below are all originally from sails-build-dictionary.
 // They are asynchronous, and besides defaulting certain options, they do a
 // handful of extra things.  So it's more than just options getting defaulted!
 //////////////////////////////////////////////////////////////////////////////
@@ -42,6 +42,7 @@ module.exports = function includeAllSync(options) {
  * WARNING: THIS PARTICULAR CONVENIENCE METHOD WILL LIKELY BE DEPRECATED.
  * (it's not actually being used anywhere in core)
  *
+ * @async
  * @param {Dictionary} options
  * @param {Function} cb
  */
@@ -56,6 +57,7 @@ module.exports.required = function(options, cb) {
  * Build a dictionary of named modules
  * (fails silently-- returns {} if the container cannot be loaded)
  *
+ * @async
  * @param {Dictionary} options
  * @param {Function} cb
  */
@@ -71,6 +73,7 @@ module.exports.optional = function(options, cb) {
  * Build a dictionary indicating whether the matched modules exist
  * (fails silently-- returns {} if the container cannot be loaded)
  *
+ * @async
  * @param {Dictionary} options
  * @param {Function} cb
  */
@@ -87,6 +90,7 @@ module.exports.exists = function(options, cb) {
  * Build a single module dictionary by extending {} with the contents of each module
  * (fail silently-- returns {} if the container cannot be loaded)
  *
+ * @async
  * @param {Dictionary} options
  * @param {Function} cb
  */
@@ -96,3 +100,55 @@ module.exports.aggregate = function(options, cb) {
   options.optional = true;
   return helpBuildDictionary(options, cb);
 };
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+// Finally, this last method is sort of like a recursive `ls`.
+// Similarly, it's more or less just a synchronous version of `.exists()`,
+// but with a few more specific hard-coded overrides.
+//////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Build a flat dictionary of the matched modules, where the keys are the
+ * paths, and the values are `true` (fails silently-- returns {} if the
+ * container cannot be loaded)
+ *
+ * @param {Dictionary} options
+ * @returns {Dictionary}
+ */
+
+module.exports.scanSync = function(options) {
+
+  // Higher level overrides.
+  options.flatten = true;
+  options.keepDirectoryPath = true;
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // Note: the combination of the following overrides make this
+  // more or less equivalent to `.exists()`-- but synchronous.
+  // Not all options work... instead, this is really designed
+  // for a slightly different kind of use case-- where you want
+  // to recursively, synchronously stat modules, rather than
+  // include them.
+  options.optional = true;
+  options.dontLoad = true;
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  // Finally, some defaults:
+  if (!options.excludeDirs) {
+    options.excludeDirs = /^\.(git|svn)$/;
+  }
+  if (!options.depth) {
+    options.depth = 10;
+  }
+  if (!options.filter) {
+    options.filter = /(.+)$/;
+  }
+
+  // Now call the low-lvl helper.
+  return helpIncludeAllSync(options);
+};
+
